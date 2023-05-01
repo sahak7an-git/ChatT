c1a386bdf38ea74e6130aa3ee2620c0dc7779a5f
sahak7an-git <sahakyanerik18@gmail.com>
2023-05-01 07:00:49 -0700

    Fixed any bugs

--- app/src/main/java/com/sahak7an/chatt/dialogs/ImageProfileDialog.java
+++ app/src/main/java/com/sahak7an/chatt/dialogs/ImageProfileDialog.java
@@ -4,12 +4,15 @@ import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,7 +20,10 @@ import androidx.fragment.app.DialogFragment;
 
 import com.makeramen.roundedimageview.RoundedImageView;
 import com.sahak7an.chatt.R;
-public class ImageProfileDialog extends DialogFragment {
+
+import java.util.Objects;
+
+public class ImageProfileDialog extends DialogFragment implements View.OnClickListener {
 
     Bitmap userImage;
 
@@ -33,6 +39,13 @@ public class ImageProfileDialog extends DialogFragment {
 
         @SuppressLint("InflateParams") View view = inflater.inflate(R.layout.dialog_user_image, null);
         RoundedImageView roundedImageView = view.findViewById(R.id.imageProfile);
+
+        Objects.requireNonNull(getDialog()).getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(0, 0, 0, 0)));
+        getDialog().getWindow().setDimAmount(8);
+
+        view.startAnimation(AnimationUtils.loadAnimation(
+                getContext(), R.anim.fade_in
+        ));
 
         roundedImageView.setImageBitmap(userImage);
 
@@ -54,17 +67,28 @@ public class ImageProfileDialog extends DialogFragment {
     @Override
     public void onDismiss(@NonNull DialogInterface dialog) {
         super.onDismiss(dialog);
+
+        dialog.dismiss();
     }
 
     @Override
     public void onCancel(@NonNull DialogInterface dialog) {
         super.onCancel(dialog);
+
+        dialog.cancel();
     }
 
     @Nullable
     @Override
     public Animation onCreateAnimation(int transit, boolean enter, int nextAnim) {
         return super.onCreateAnimation(transit, enter, nextAnim);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        dismiss();
+
     }
 
 }
